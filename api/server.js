c8176f7d4a98c972c933841c68fef00f18456b2c
gdmcv <gdmcvittie@users.noreply.github.com>
2026-07-14 09:00:43 -0400

    fixed freesound search results

--- api/server.js
+++ api/server.js
@@ -963,12 +963,13 @@ app.get('/proxy-oga', async (req, res) => {
     }
     targetUrl = parsedUrl.href;
 
+    const originUrl = `https://${hostname}`;
     const requestHeaders = {
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
       'Accept-Language': 'en-US,en;q=0.5',
       'Upgrade-Insecure-Requests': '1',
-      'Referer': 'https://modarchive.org/'
+      'Referer': originUrl + '/'
     };
 
     // Instantiate the timeout controller early and increase it to 45 seconds.
